3d9c9612bc9939c9ed194f5256da097c77f23097
90554 <905541529@qq.com>
2025-07-10 00:32:08 +0800

    Changes

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,6 +16,23 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        // 腾讯云镜像（新增）
+        maven { url "https://mirrors.cloud.tencent.com/nexus/repository/maven-public/" }
+        // 华为云（首推）
+        maven { url "https://mirrors.huaweicloud.com/repository/maven/" }
+        // 清华大学源（次选）
+        maven { url "https://mirrors.tuna.tsinghua.edu.cn/nexus/content/repositories/maven-central/" }
+        // 阿里云（备用）
+        maven { url "https://maven.aliyun.com/repository/public" }
+        // 其他镜像或官方仓库
+         google()
+         mavenCentral()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
