70b176b3cb05c8b7aba13ca3167c2ab0ad87f692
wth19960304 <46277517+wth19960304@users.noreply.github.com>
2025-07-10 20:25:14 +0800

    Add files via upload

---
+++ lib/constant/length.dart
@@ -0,0 +1,7 @@
+class Klength {
+  static const double topBarHeight = 45;
+  static const double bottomBarHeight = 56;
+  static const int designWidth = 360; //设计稿宽度
+  static const int designHight = 640; //设计稿宽度
+  static const double searchTxtFieldHeight = 34;
+}
