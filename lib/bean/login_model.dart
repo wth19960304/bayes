a4937af97ad2e70275a5257042ce5e9013b1ebea
wth19960304 <46277517+wth19960304@users.noreply.github.com>
2025-07-15 20:39:48 +0800

    Update login_model.dart

--- lib/bean/login_model.dart
+++ lib/bean/login_model.dart
@@ -1,38 +1,38 @@
 class LoginBean {
-  int state;
-  String message;
-  Data data;
+  int? state;
+  String? message;
+  Data? data; // 将data标记为可空
 
   LoginBean({this.state, this.message, this.data});
 
   LoginBean.fromJson(Map<String, dynamic> json) {
     state = json['state'];
     message = json['message'];
-    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['state'] = this.state;
-    data['message'] = this.message;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['state'] = state;
+    data['message'] = message;
     if (this.data != null) {
-      data['data'] = this.data.toJson();
+      data['data'] = this.data!.toJson();
     }
     return data;
   }
 }
 
 class Data {
-  User user;
-  String token;
-  String openId;
-  String nickname;
-  String headImg;
+  User? user;
+  String? token;
+  String? openId;
+  String? nickname;
+  String? headImg;
 
   Data({this.user, this.token, this.openId, this.nickname, this.headImg});
 
   Data.fromJson(Map<String, dynamic> json) {
-    user = json['user'] != null ? new User.fromJson(json['user']) : null;
+    user = (json['user'] != null ? User.fromJson(json['user']) : null)!;
     token = json['token'];
     openId = json['openid'];
     nickname = json['nickname'];
@@ -40,65 +40,64 @@ class Data {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.user != null) {
-      data['user'] = this.user.toJson();
-    }
-    data['token'] = this.token;
-    data['openid'] = this.openId;
-    data['nickname'] = this.nickname;
-    data['headImg'] = this.headImg;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['user'] = user?.toJson();
+    data['token'] = token;
+    data['openid'] = openId;
+    data['nickname'] = nickname;
+    data['headImg'] = headImg;
     return data;
   }
 }
 
 class User {
-  int id;
-  String password;
-  String phoneNum;
-  String nickname;
-  String sex;
-  String grade;
-  String userType;
-  String division;
-  String mathScores;
-  String provincialId;
-  String provincialName;
-  String cityId;
-  String cityName;
-  String areaId;
-  String areaName;
-  String position;
-  String state;
-  String addTime;
-  String createBy;
-  String createTime;
-  String updateBy;
-  String updateTime;
+  int? id;
+  String? password;
+  String? phoneNum;
+  String? nickname;
+  String? sex;
+  String? grade;
+  String? userType;
+  String? division;
+  String? mathScores;
+  String? provincialId;
+  String? provincialName;
+  String? cityId;
+  String? cityName;
+  String? areaId;
+  String? areaName;
+  String? position;
+  String? state;
+  String? addTime;
+  String? createBy;
+  String? createTime;
+  String? updateBy;
+  String? updateTime;
 
-  User(
-      {this.id,
-      this.password,
-      this.phoneNum,
-      this.nickname,
-      this.sex,
-      this.grade,
-      this.userType,
-      this.division,
-      this.mathScores,
-      this.provincialId,
-      this.provincialName,
-      this.cityId,
-      this.cityName,
-      this.areaId,
-      this.areaName,
-      this.position,
-      this.state,
-      this.addTime,
-      this.createBy,
-      this.createTime,
-      this.updateBy,
-      this.updateTime});
+  User({
+    this.id,
+    this.password,
+    this.phoneNum,
+    this.nickname,
+    this.sex,
+    this.grade,
+    this.userType,
+    this.division,
+    this.mathScores,
+    this.provincialId,
+    this.provincialName,
+    this.cityId,
+    this.cityName,
+    this.areaId,
+    this.areaName,
+    this.position,
+    this.state,
+    this.addTime,
+    this.createBy,
+    this.createTime,
+    this.updateBy,
+    this.updateTime,
+  });
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -126,29 +125,29 @@ class User {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['password'] = this.password;
-    data['phoneNum'] = this.phoneNum;
-    data['nickname'] = this.nickname;
-    data['sex'] = this.sex;
-    data['grade'] = this.grade;
-    data['userType'] = this.userType;
-    data['division'] = this.division;
-    data['mathScores'] = this.mathScores;
-    data['provincialId'] = this.provincialId;
-    data['provincialName'] = this.provincialName;
-    data['cityId'] = this.cityId;
-    data['cityName'] = this.cityName;
-    data['areaId'] = this.areaId;
-    data['areaName'] = this.areaName;
-    data['position'] = this.position;
-    data['state'] = this.state;
-    data['addTime'] = this.addTime;
-    data['createBy'] = this.createBy;
-    data['createTime'] = this.createTime;
-    data['updateBy'] = this.updateBy;
-    data['updateTime'] = this.updateTime;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['password'] = password;
+    data['phoneNum'] = phoneNum;
+    data['nickname'] = nickname;
+    data['sex'] = sex;
+    data['grade'] = grade;
+    data['userType'] = userType;
+    data['division'] = division;
+    data['mathScores'] = mathScores;
+    data['provincialId'] = provincialId;
+    data['provincialName'] = provincialName;
+    data['cityId'] = cityId;
+    data['cityName'] = cityName;
+    data['areaId'] = areaId;
+    data['areaName'] = areaName;
+    data['position'] = position;
+    data['state'] = state;
+    data['addTime'] = addTime;
+    data['createBy'] = createBy;
+    data['createTime'] = createTime;
+    data['updateBy'] = updateBy;
+    data['updateTime'] = updateTime;
     return data;
   }
 }
