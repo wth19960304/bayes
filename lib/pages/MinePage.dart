4e99f41e7809e14855ade3e2a247fd9e9ae48f85
wth19960304 <46277517+wth19960304@users.noreply.github.com>
2025-07-24 20:57:02 +0800

    Update MinePage.dart

--- lib/pages/MinePage.dart
+++ lib/pages/MinePage.dart
@@ -1,14 +1,31 @@
 import 'dart:io';
 
+import 'package:bayes/base/base_inner_widget.dart';
+import 'package:bayes/bean/UserInfoBean.dart';
+import 'package:bayes/constant/color.dart';
+import 'package:bayes/constant/font.dart';
+import 'package:bayes/loginRegister/login_page.dart';
+import 'package:bayes/network/intercept/showloading_intercept.dart';
+import 'package:bayes/network/requestUtil.dart';
+import 'package:bayes/pages/MyMessagePage.dart';
+import 'package:bayes/pages/tv_page.dart';
+import 'package:bayes/userInfo/ChengjiuPage.dart';
+import 'package:bayes/userInfo/FirstUserInfoPage.dart';
+import 'package:bayes/userInfo/MeInfoPage.dart';
+import 'package:bayes/utils/screen_util.dart';
+import 'package:bayes/widget/image_loading.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:dio/dio.dart';
-
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 ///个人中心
+// ignore: must_be_immutable
 class MinePage extends BaseInnerWidget {
+  MinePage({super.key});
+
   @override
+  // ignore: override_on_non_overriding_member
   int setIndex() {
     return 2;
   }
@@ -24,9 +41,6 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
 
   @override
   Widget buildWidget(BuildContext context) {
-    if (data == null) {
-      return baseStatueWidget(LoadingWidgetStatue.LOADING);
-    }
     String userInfo = "";
     if (data.grade != "") {
       userInfo += "${data.grade} | ";
@@ -46,18 +60,18 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
               children: <Widget>[
                 _headerWidget(),
                 Container(
-                  margin: EdgeInsets.all(ScreenUtil().L(10)),
+                  margin: EdgeInsets.all(ScreenUtil.L(10)),
                   child: Text(
                     "${data.nickname}",
-                    style: KFontConstant.appBarTiltleBig_bold(),
+                    style: KFontConstant.appBarTiltleBigBold(),
                   ),
                 ),
-                Text("$userInfo", style: KFontConstant.greyTextBig()),
+                Text(userInfo, style: KFontConstant.greyTextBig()),
                 Padding(
                   padding: EdgeInsets.only(
-                    left: ScreenUtil().L(50),
-                    right: ScreenUtil().L(50),
-                    top: ScreenUtil().L(10),
+                    left: ScreenUtil.L(50),
+                    right: ScreenUtil.L(50),
+                    top: ScreenUtil.L(10),
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -76,13 +90,13 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
             alignment: Alignment.topRight,
             child: InkWell(
               onTap: () {
-                _routePage(98);
+                _routePage(98, type: null);
               },
               child: Padding(
-                padding: EdgeInsets.all(ScreenUtil().L(10)),
+                padding: EdgeInsets.all(ScreenUtil.L(10)),
                 child: Image.asset(
                   "images/message_icon.png",
-                  width: ScreenUtil().L(25),
+                  width: ScreenUtil.L(25),
                 ),
               ),
             ),
@@ -94,51 +108,46 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
 
   //学习记录
   _topWidget(int index) {
-    List<String> title = new List();
+    List<String> title = [];
     title.add("学习记录");
     title.add("练习记录");
     title.add("我的缓存");
-    List<String> images = new List();
+    List<String> images = [];
     images.add("images/minekuang1.png");
     images.add("images/minekuang2.png");
     images.add("images/minekuang3.png");
-    List<String> nums = List();
+    List<String> nums = [];
     nums.add("${data.studyNum}");
     nums.add("${data.testNum}");
-    nums.add("${pathNum}");
+    nums.add("$pathNum");
     return InkWell(
       onTap: () {
-        _routePage(100 + index);
+        _routePage(100 + index, type: null);
       },
-      child: Container(
-        child: Stack(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.topCenter,
-              child: Image.asset(
-                "${images[index]}",
-                height: ScreenUtil().L(70),
-              ),
-            ),
-            Container(
-              height: ScreenUtil().L(65),
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.all(ScreenUtil().L(5)),
-                    child: Text(
-                      "${nums[index]}",
-                      style: KFontConstant.appBarTiltleBig_bold(),
-                    ),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topCenter,
+            child: Image.asset(images[index], height: ScreenUtil.L(70)),
+          ),
+          Container(
+            height: ScreenUtil.L(65),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(ScreenUtil.L(5)),
+                  child: Text(
+                    nums[index],
+                    style: KFontConstant.appBarTiltleBigBold(),
                   ),
-                  Text("${title[index]}", style: KFontConstant.grayText()),
-                ],
-              ),
+                ),
+                Text(title[index], style: KFontConstant.grayText()),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -147,39 +156,34 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
   _headerWidget() {
     return InkWell(
       onTap: () {
-        _routePage(99);
+        _routePage(99, type: null);
       },
       child: Stack(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(top: ScreenUtil().L(30)),
+            margin: EdgeInsets.only(top: ScreenUtil.L(30)),
             alignment: Alignment.topCenter,
-            child: Image.asset(
-              "images/headerbg.png",
-              height: ScreenUtil().L(90),
-            ),
+            child: Image.asset("images/headerbg.png", height: ScreenUtil.L(90)),
           ),
           Container(
-            margin: EdgeInsets.only(top: ScreenUtil().L(30)),
+            margin: EdgeInsets.only(top: ScreenUtil.L(30)),
             alignment: Alignment.center,
-            height: ScreenUtil().L(90),
+            height: ScreenUtil.L(90),
             child: ClipRRect(
-              borderRadius: BorderRadius.all(
-                Radius.circular(ScreenUtil().L(50)),
-              ),
+              borderRadius: BorderRadius.all(Radius.circular(ScreenUtil.L(50))),
               child: CachedNetworkImage(
                 imageUrl:
-                    "${data.headImg == null || data.headImg.length == 0 ? "" : data.headImg[0].url}",
+                    "${data.headImg == null || data.headImg?.length == 0 ? "" : data.headImg?[0].url}",
                 placeholder: (context, url) => ImageLoadingPage(width: 20.0),
                 errorWidget: (context, url, error) => Image.asset(
                   "images/header_defalut.png",
                   fit: BoxFit.cover,
-                  width: ScreenUtil().L(90),
-                  height: ScreenUtil().L(90),
+                  width: ScreenUtil.L(90),
+                  height: ScreenUtil.L(90),
                 ),
                 fit: BoxFit.cover,
-                width: ScreenUtil().L(90),
-                height: ScreenUtil().L(90),
+                width: ScreenUtil.L(90),
+                height: ScreenUtil.L(90),
               ),
             ),
           ),
@@ -191,11 +195,11 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
   //底部布局
   _buttom() {
     return Container(
-      margin: EdgeInsets.only(top: ScreenUtil().L(20)),
+      margin: EdgeInsets.only(top: ScreenUtil.L(20)),
       padding: EdgeInsets.only(
-        left: ScreenUtil().L(10),
-        bottom: ScreenUtil().L(50),
-        top: ScreenUtil().L(10),
+        left: ScreenUtil.L(10),
+        bottom: ScreenUtil.L(50),
+        top: ScreenUtil.L(10),
       ),
       color: KColorConstant.appBgColor,
       child: Column(
@@ -209,7 +213,7 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
           _rowItemView("账户注销", "images/mine_huiyuan.png", 1),
           Container(
             color: Colors.transparent,
-            margin: EdgeInsets.only(top: ScreenUtil().L(30)),
+            margin: EdgeInsets.only(top: ScreenUtil.L(30)),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -217,9 +221,7 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
                   onTap: () {
                     Navigator.push(
                       context,
-                      new MaterialPageRoute(
-                        builder: (context) => new MeInfoPage(),
-                      ),
+                      MaterialPageRoute(builder: (context) => MeInfoPage()),
                     );
                   },
                   child: Text("《用户协议》", style: KFontConstant.themeText()),
@@ -249,10 +251,10 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
   }) {
     return InkWell(
       onTap: () {
-        _routePage(index);
+        _routePage(index, type: null);
       },
-      child: Container(
-        height: ScreenUtil().L(52),
+      child: SizedBox(
+        height: ScreenUtil.L(52),
         width: double.infinity,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -260,13 +262,13 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
             Row(
               children: <Widget>[
                 Container(
-                  height: ScreenUtil().L(50),
-                  width: ScreenUtil().L(50),
+                  height: ScreenUtil.L(50),
+                  width: ScreenUtil.L(50),
                   padding: EdgeInsets.only(
-                    left: ScreenUtil().L(12),
-                    right: ScreenUtil().L(12),
-                    top: ScreenUtil().L(paddingH),
-                    bottom: ScreenUtil().L(paddingH),
+                    left: ScreenUtil.L(12),
+                    right: ScreenUtil.L(12),
+                    top: ScreenUtil.L(paddingH),
+                    bottom: ScreenUtil.L(paddingH),
                   ),
                   child: Image.asset(imageUrl),
                 ),
@@ -274,9 +276,9 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
               ],
             ),
             Container(
-              height: ScreenUtil().L(56),
-              width: ScreenUtil().L(56),
-              padding: EdgeInsets.all(ScreenUtil().L(20)),
+              height: ScreenUtil.L(56),
+              width: ScreenUtil.L(56),
+              padding: EdgeInsets.all(ScreenUtil.L(20)),
               child: Image.asset("images/right_go.png"),
             ),
           ],
@@ -286,12 +288,13 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
   }
 
   ///界面跳转
-  _routePage(int index, {int type}) {
+  _routePage(int index, {int? type}) {
     //学习成就
     if (index == 0) {
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new ChengjiuPage()),
+        // ignore: unnecessary_
+        MaterialPageRoute(builder: (context) => ChengjiuPage()),
       );
     }
     if (index == 1) {
@@ -299,20 +302,20 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
-          return new AlertDialog(
-            title: new Text('账户注销'),
-            content: new SingleChildScrollView(
-              child: new Text('您确定要申请注销该账户吗？账户注销后，该账号将不可登录本APP，并清除您之前的账户数据。'),
+          return AlertDialog(
+            title: Text('账户注销'),
+            content: SingleChildScrollView(
+              child: Text('您确定要申请注销该账户吗？账户注销后，该账号将不可登录本APP，并清除您之前的账户数据。'),
             ),
             actions: <Widget>[
-              new FlatButton(
-                child: new Text('取消'),
+              TextButton(
+                child: Text('取消'),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
               ),
-              new FlatButton(
-                child: new Text('确定'),
+              TextButton(
+                child: Text('确定'),
                 onPressed: () {
                   showToast("您的账号注销已提交，等待后台处理。");
                   Navigator.of(context).pop();
@@ -327,42 +330,49 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
     } else if (index == 2) {
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new ShouCangPage()),
+        MaterialPageRoute(
+          builder: (context) =>
+              // ShouCangPage()
+              Text("wth"),
+        ),
       );
     } else if (index == 3) {
-      showShareDialog(title: "贝叶斯数学，内容全免费哟，快来下载和我一起学习吧！", type: "app");
+      showShareDialog(
+        title: "贝叶斯数学，内容全免费哟，快来下载和我一起学习吧！",
+        type: "app",
+        id: '',
+        videoType: '',
+      );
     } else if (index == 4) {
       showToast("已是最新版本");
     } else if (index == 5) {
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new MeInfoPage()),
+        MaterialPageRoute(builder: (context) => MeInfoPage()),
       );
     } else if (index == 6) {
       showDialog<Null>(
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
-          return new AlertDialog(
-            title: new Text('退出登录'),
-            content: new SingleChildScrollView(child: new Text('您确定退出该账号吗？')),
+          return AlertDialog(
+            title: Text('退出登录'),
+            content: SingleChildScrollView(child: Text('您确定退出该账号吗？')),
             actions: <Widget>[
-              new FlatButton(
-                child: new Text('取消'),
+              TextButton(
+                child: Text('取消'),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
               ),
-              new FlatButton(
-                child: new Text('确定'),
+              TextButton(
+                child: Text('确定'),
                 onPressed: () {
                   Navigator.of(context).pop();
                   Navigator.of(context).pop();
                   Navigator.push(
                     context,
-                    new MaterialPageRoute(
-                      builder: (context) => new LoginPage(),
-                    ),
+                    MaterialPageRoute(builder: (context) => LoginPage()),
                   );
                 },
               ),
@@ -376,30 +386,42 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
       //消息中心
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new MyMessagePage()),
+        MaterialPageRoute(builder: (context) => MyMessagePage()),
       );
     } else if (index == 99) {
       //头像 - 个人中心
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new FirstUserInfoPage()),
+        MaterialPageRoute(builder: (context) => FirstUserInfoPage()),
       );
     } else if (index == 100) {
       //学习记录
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new XuexiJiluPage()),
+        MaterialPageRoute(
+          builder: (context) =>
+              //  XuexiJiluPage()
+              Text("wth"),
+        ),
       );
     } else if (index == 101) {
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new ShitiListPage()),
+        MaterialPageRoute(
+          builder: (context) =>
+              // ShitiListPage()
+              Text("wth"),
+        ),
       );
     } else if (index == 102) {
       //缓存列表
       Navigator.push(
         context,
-        new MaterialPageRoute(builder: (context) => new XiazaiPage()),
+        MaterialPageRoute(
+          builder: (context) =>
+              //  XiazaiPage()
+              Text("wth"),
+        ),
       );
     }
   }
@@ -412,15 +434,15 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
   }
 
   _initFluwx() async {
-    await fluwx.registerWxApi(
-      appId: "wxa4a5f5d0d85630a2",
-      doOnAndroid: true,
-      doOnIOS: true,
-    );
+    // await fluwx.registerWxApi(
+    //   appId: "wxa4a5f5d0d85630a2",
+    //   doOnAndroid: true,
+    //   doOnIOS: true,
+    // );
   }
 
   _initFluQQ() async {
-    FlutterQq.registerQQ('101887594');
+    // FlutterQq.registerQQ('101887594');
   }
 
   @override
@@ -443,12 +465,12 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
 
   @override
   double getVerticalMargin() {
-    return ScreenUtil().L(50) + //bar栏高度
-        ScreenUtil.statusBarHeight + //状态栏高度
-        ScreenUtil.bottomBarHeight;
+    return ScreenUtil.L(50) +
+        (ScreenUtil.statusBarHeight ?? 0) +
+        (ScreenUtil.bottomBarHeight ?? 0);
   }
 
-  UserInfoData data;
+  late UserInfoData data;
 
   int first = 0;
 
@@ -461,14 +483,12 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
     ).listen(
       (data) {
         setState(() {
-          this.data = data.data;
+          this.data = data.data!;
           if (this.data.position == "") {
             //完善个人信息
             Navigator.push(
               context,
-              new MaterialPageRoute(
-                builder: (context) => new FirstUserInfoPage(),
-              ),
+              MaterialPageRoute(builder: (context) => FirstUserInfoPage()),
             );
           }
         });
@@ -479,14 +499,14 @@ class _MinePageState extends BaseInnerWidgetState<MinePage> {
     );
   }
 
-  String sDCardDir;
+  late String sDCardDir;
 
   int pathNum = 0;
 
   _getData() async {
     // 打印出test文件夹下文件的路径
-    sDCardDir = (await getExternalStorageDirectory()).path;
-    Directory directory = new Directory('$sDCardDir/videoBys');
+    sDCardDir = (await getExternalStorageDirectory())!.path;
+    Directory directory = Directory('$sDCardDir/videoBys');
 
     pathNum = directory.listSync().length;
   }
